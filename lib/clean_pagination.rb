28b026ede15ed639eaeee327c463f577d51575a3
Joe Nelson <cred+github@begriffs.com>
2014-02-03 03:15:50 -0500

    Closer to spec

--- lib/clean_pagination.rb
+++ lib/clean_pagination.rb
@@ -12,21 +12,17 @@ module CleanPagination
       end
     end
 
+    to     = [total_items - 1, to].min
     limit  = to - from + 1
     offset = from
 
-    if limit > max_range_size
-      response.status = 413
-      return
-    end
-    if to <= from || to >= total_items
+    if from >= to
       response.status = 416
+      headers['Content-Range'] = "*/#{total_items}"
       return
     end
 
-    yield limit, offset
-
-    if limit < total_items
+    if limit <= max_range_size
       display_total = if total_items < Float::INFINITY
                         total_items
                       else
@@ -35,7 +31,21 @@ module CleanPagination
       headers['Range-Unit'] = 'items'
       headers['Content-Range'] =
         "#{from}-#{to}/#{display_total}"
-      response.status = 206 if response.status == 200
+
+      yield limit, offset
+      response.status = 206 if limit < total_items && response.status == 200
+
+      links = []
+      links << "<#{request.path}>; rel=\"first\"; items=\"0-#{limit-1}\""
+      links << "<#{request.path}>; rel=\"last\"; items=\"#{[to + 1, total_items - limit].max}-#{total_items - 1}\""
+      links << "<#{request.path}>; rel=\"next\"; items=\"#{to + 1}-#{[total_items - 1, to + limit].min}\""
+      links << "<#{request.path}>; rel=\"prev\"; items=\"#{[0, from - limit].max}-#{[from - 1, limit-1].max}\""
+
+      headers['Link'] = links.join ', '
+    else
+      response.status = 413
+      headers['Content-Range'] = "*/#{total_items}"
     end
+
   end
 end
--- test/clean_pagination_test.rb
+++ test/clean_pagination_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
+def parse_link_ranges header
+  links = Hash.new
+  (header || '').split(',').each do |link|
+    link =~ /rel="(.*)".*items="(.*)"/
+    links[$1] = $2
+  end
+  links
+end
+
 class ApplicationControllerTest < ActionController::TestCase
   setup do
     @controller.stubs(:total_items).returns 101
@@ -20,6 +29,7 @@ class ApplicationControllerTest < ActionController::TestCase
     get :index
     assert_equal 413, response.status
     assert_equal 'items', response.headers['Accept-Ranges']
+    assert_equal '*/101', response.headers['Content-Range']
   end
 
   test 'an acceptable range succeeds' do
@@ -69,14 +79,58 @@ class ApplicationControllerTest < ActionController::TestCase
     @controller.expects(:action).never
     get :index
     assert_equal 416, response.status
+    assert_equal '*/101', response.headers['Content-Range']
   end
 
-  test "refuses ranges beyond collection" do
+  test "handles ranges beyond collection length via truncation" do
     @request.headers['Range-Unit'] = 'items'
-    @request.headers['Range'] = "50-101"
+    @request.headers['Range'] = "50-200"
 
-    @controller.expects(:action).never
+    @controller.expects(:action).with(51, 50)
     get :index
-    assert_equal 416, response.status
+
+    assert_equal 206, response.status
+    assert_equal '50-100/101', response.headers['Content-Range']
+  end
+
+  test "includes link headers" do
+    @controller.stubs(:total_items).returns 100
+
+    @request.headers['Range-Unit'] = 'items'
+    @request.headers['Range'] = "20-29"
+
+    get :index
+    links = parse_link_ranges response.headers['Link']
+
+    assert_equal '30-39', links['next']
+    assert_equal '10-19', links['prev']
+    assert_equal '0-9', links['first']
+    assert_equal '90-99', links['last']
+  end
+
+  test "crops next page at end, shortening range" do
+    @controller.stubs(:total_items).returns 100
+
+    @request.headers['Range-Unit'] = 'items'
+    @request.headers['Range'] = "50-89"
+
+    get :index
+    links = parse_link_ranges response.headers['Link']
+
+    assert_equal '90-99', links['next']
+    assert_equal '90-99', links['last']
+  end
+
+  test "shifts penultimate page to beginning, preserving length" do
+    @controller.stubs(:total_items).returns 100
+
+    @request.headers['Range-Unit'] = 'items'
+    @request.headers['Range'] = "10-49"
+
+    get :index
+    links = parse_link_ranges response.headers['Link']
+
+    assert_equal '0-39', links['prev']
+    assert_equal '0-39', links['first']
   end
 end
